a6bfca08ad3b80adba767f5f6cba62dda12d49d6
Gwenaëlle Bussac <gwen.bussac@gmail.com>
2025-01-24 10:41:00 +0100

    Step 1.2 finish

--- sapin.js
+++ sapin.js
@@ -2,14 +2,24 @@ function afficherPointeSapin(hauteur) {
 
 }
 
-afficherPointeSapin(4);
+// afficherPointeSapin(4);
 
 function afficherEtoiles(n) {
     // console.log(n);
     const etoile = "*"
     let nbrEnEtoiles = etoile.repeat(n);
-    console.log(nbrEnEtoiles);
+    // console.log(nbrEnEtoiles);
+    return nbrEnEtoiles
 }
 
-afficherEtoiles(2);
-afficherEtoiles(5);
+// afficherEtoiles(2);
+// afficherEtoiles(5);
+
+function afficherRectangle(hauteur, largeur) {
+    for (let i = 0; i < hauteur ; i++) {
+        let etages = afficherEtoiles(largeur);
+        console.log(etages);
+    }
+}
+
+afficherRectangle(5,5);
