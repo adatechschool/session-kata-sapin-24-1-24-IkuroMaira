ce5a319a88f31049d451477afe91c1237faf26b4
Gwenaëlle Bussac <gwen.bussac@gmail.com>
2025-01-28 14:53:00 +0100

    Add correction

--- sapin.js
+++ sapin.js
@@ -1,9 +1,3 @@
-function afficherPointeSapin(hauteur) {
-
-}
-
-// afficherPointeSapin(4);
-
 function afficherEtoiles(n) {
     // console.log(n);
     const etoile = "*"
@@ -12,6 +6,7 @@ function afficherEtoiles(n) {
     return nbrEnEtoiles
 }
 
+// afficherEtoiles(1); // Les nombres négatif marche par avec la méthode repeat
 // afficherEtoiles(2);
 // afficherEtoiles(5);
 
@@ -41,13 +36,62 @@ function afficherTriangleGauche(n) {
         let etage = afficherEtoiles(j);
         let espace = " ";
 
-        if (j === -1) {
+        if (j === 0) {
             console.log(espace.repeat(n), "/");
-        } else if (j > -1) {
+        } else if (j > 0) {
             let k = n - j
-            console.log(espace.repeat(k), "/",etage,);
+            console.log(espace.repeat(k), "/", etage);
         }
     }
 }
 
-afficherTriangleGauche(5);
+// afficherTriangleGauche(5);
+
+function afficherPointeSapin(n) {
+    let j = 0;
+
+    for (let i = 0; i < n; i++) {
+        j++
+        // console.log(j);
+
+        let etage = afficherEtoiles(j);
+        let espace = " ";
+
+        if (j === 1) {
+            console.log(espace.repeat(n-2), "+");
+        } else if (j === 2) {
+            let k = n - j
+            console.log(`${espace.repeat(k)}/|\\`);
+        } else if (j > 2) {
+            let k = n - j
+            // console.log(n, j, k);
+
+            etage = etage.slice(2);
+            console.log(`${espace.repeat(k)}/${etage}|${etage}\\`);
+        }
+    }
+}
+
+// CORRECTION COURS :
+function afficherPointeSapin(hauteur) {
+    for (let i = 0; i < hauteur; i++) {
+        console.log(' '.repeat(hauteur) + "+");
+        for (let ligne = 1; ligne < hauteur; ligne++) {
+            let droite = '*'.repeat(ligne - 1) + '\\';
+            let gauche = ' '.repeat(hauteur - ligne) + '/' + '*'.repeat(ligne - 1);
+            console.log(gauche + '|' + droite);
+        }
+    }
+}
+
+// afficherPointeSapin(2);
+// afficherPointeSapin(3);
+// afficherPointeSapin(4);
+
+function afficherEtage(hauteur, pointe_offset) {
+
+}
+
+afficherEtage(3, 0);
+afficherEtage(3, 1);
+afficherEtage(3, 2);
